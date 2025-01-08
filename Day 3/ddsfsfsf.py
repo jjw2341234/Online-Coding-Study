e11b66d25cf80548bee263fa185c9a30997d5d55
jjw2341234 <jjw2341234@gmail.com>
2025-01-08 17:44:09 +0900

    Create ddsfsfsf.py
    브루트 포스

---
+++ Day 3/ddsfsfsf.py
@@ -0,0 +1,15 @@
+n = int(input())
+visit = [0] * int(1e6+1)
+visit[1] = 0
+visit[2] = 1
+if n <= 2:
+    print(visit[n])
+    exit(0)
+else:
+    for i in range(2, n+1):
+        visit[i] = visit[i-1] + 1
+        if i % 3 == 0:
+            visit[i] = min(visit[i], visit[i//3] +1)
+        if i % 2 == 0:
+            visit[i] = min(visit[i], visit[i//2] + 1)
+print(visit[n])
