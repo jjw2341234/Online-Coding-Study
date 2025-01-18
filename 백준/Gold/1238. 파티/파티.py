8e53c418d4cc107bfc8ae2ae67ffac9bd4d05f45
jjw2341234 <jjw2341234@gmail.com>
2025-01-18 16:40:10 +0900

    [Gold III] Title: 파티, Time: 596 ms, Memory: 117936 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,4 +1,4 @@
-import heapq
+from collections import deque
 n,m,x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
@@ -6,18 +6,19 @@ for _ in range(m):
     graph[a].append([b,c])
 
 def bfs(start, end):
-    q = [[0, start]]
-    visit = [int(1e10)] * (n+1)
+    q = deque()
+    q.append([start, 0])
+    visit = [1e10] * (n+1)
     visit[start] = 0
     while q:
-        cost, node = heapq.heappop(q)
+        node, cost = q.popleft()
         if visit[node] < cost:
             continue
         for next, dist in graph[node]:
             cal = dist + cost
             if visit[next] > cal:
                 visit[next] = cal
-                heapq.heappush(q, [cal, next])
+                q.append([next, cal])
     return visit[end]
 result = 0
 for i in range(1, n+1):
