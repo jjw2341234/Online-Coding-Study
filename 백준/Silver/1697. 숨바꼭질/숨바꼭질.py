4bb1b9ae47911543c8ed377a1c9b1ea2c539cbac
jjw2341234 <jjw2341234@gmail.com>
2025-01-21 11:24:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 120 ms, Memory: 115092 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -5,7 +5,7 @@ def bfs(x):
     q.append(x)
     while q:
         x = q.popleft()
-        if x == e:
+        if visit[e]:
             return visit[e]
         for nx in [x-1, x+1, 2*x]:
             if 0<=nx< 100001 and visit[nx] == 0:
@@ -13,5 +13,8 @@ def bfs(x):
                 q.append(nx)
 
 s,e = map(int, input().split())
+if s == e:
+    print(0)
+    exit(0)
 visit = [0] * (100001)
 print(bfs(s))
