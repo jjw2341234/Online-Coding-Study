b2cab7b9d4efb512210093eac32c88aba9a4d1a5
jjw2341234 <jjw2341234@gmail.com>
2025-01-29 19:13:14 +0900

    [Gold IV] Title: 서강그라운드, Time: 108 ms, Memory: 110840 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,22 +1,23 @@
 n,m,r = map(int, input().split())
 area = list(map(int,input().split()))
-INF=1e9
+INF = int(1e9)
 graph = [[INF] * (n+1) for _ in range(n+1)]
-for _ in range(r):
-    a,b,c = map(int, input().split())
-    graph[a][b],graph[b][a]=c,c
 for i in range(1, n+1):
     graph[i][i] = 0
-
+for _ in range(r):
+    a,b,c = map(int, input().split())
+    graph[a][b] = graph[b][a] = c
 for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            
 res = 0
+
 for i in range(1, n+1):
     s = 0
     for j in range(1, n+1):
-        if graph[i][j] <= m:
+        if graph[i][j] <=m:
             s+= area[j-1]
     res = max(s, res)
 print(res)
