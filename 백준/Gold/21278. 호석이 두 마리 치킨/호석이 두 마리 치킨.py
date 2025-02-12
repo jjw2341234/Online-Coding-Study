8c96b40ca1c0bf70209de4b3660319d9add98b8b
jjw2341234 <jjw2341234@gmail.com>
2025-02-12 13:27:09 +0900

    [Gold IV] Title: 호석이 두 마리 치킨, Time: 132 ms, Memory: 111412 KB, Score: 6 point -BaekjoonHub

---
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -0,0 +1,27 @@
+n,m = map(int, input().split())
+
+graph = [[int(1e9)] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+res = int(1e9)
+
+for i in range(1, n):
+    tmp = 0
+    for j in range(2, n+1):
+        for k in range(1, n+1):
+            tmp += min(graph[i][k], graph[j][k]) * 2
+        if tmp < res:
+            res = tmp
+            print(i,j,tmp)
