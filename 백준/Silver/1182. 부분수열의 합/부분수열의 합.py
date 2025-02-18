161ee47b7a2261e770402d4ec967236be3d52854
jjw2341234 <jjw2341234@gmail.com>
2025-02-18 17:01:59 +0900

    [Silver II] Title: 부분수열의 합, Time: 1344 ms, Memory: 215096 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,20 +1,21 @@
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-def dfs(idx, path, r, res):
+def dfs(idx, path, r):
     # 길이가 r인 조합을 찾으면 res에 추가
+    global res
     if len(path) == r:
         res.append(path)
         return
     
     # 조합을 생성하기 위해 재귀 호출
     for i in range(idx, len(arr)):
-        dfs(i + 1, path + [arr[i]], r, res)  # 현재 원소를 추가하여 재귀 호출
+        dfs(i + 1, path + [arr[i]], r)  # 현재 원소를 추가하여 재귀 호출
 
 cnt = 0
 for i in range(1, n+1):
     res = []
-    dfs(0, [], i, res)  # 각 길이에 대해 dfs 호출
+    dfs(0, [], i)  # 각 길이에 대해 dfs 호출
     for x in res:
         if sum(x) == s:
             cnt += 1
