0b37baf8b9e5c105fed2836d79ec0012f2f80f65
jjw2341234 <jjw2341234@gmail.com>
2025-02-19 17:18:36 +0900

    [Silver III] Title: 모든 순열, Time: 164 ms, Memory: 113968 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,12 +1,12 @@
 n = int(input())
 arr = []
-def dfs(lev):
-    if lev == n:
+def dfs():
+    if len(arr) == n:
         print(*arr)
         return
     for i in range(1, n+1):
         if i not in arr:
             arr.append(i)
-            dfs(lev+1)
+            dfs()
             arr.pop()
-dfs(0)
+dfs()
