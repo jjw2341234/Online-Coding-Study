329088b4a5ad20045eaa5d8c235eaa5f0b2ad303
jjw2341234 <jjw2341234@gmail.com>
2025-02-26 11:16:53 +0900

    [level 3] Title: 블록 이동하기, Time: 68.34 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
@@ -1,44 +1,41 @@
 from collections import deque
-    
-def move(p1, p2, road):
-    
-    dirs = [(0,1),(1,0),(-1,0),(0,-1)]
-    poss = []
-    # 상하좌우로 이동
-    for dx, dy in dirs:
-        n1 = (p1[0] + dx, p1[1] + dy)
-        n2 = (p2[0] + dx, p2[1] + dy)
+def move(q1, q2, road):
+    dir = [(-1,0), (1,0), (0,1), (0,-1)]
+    pos = []
+    for dx, dy in dir:
+        n1 = (q1[0] + dx, q1[1] + dy)
+        n2 = (q2[0] + dx, q2[1] + dy)
+        
         if road[n1[0]][n1[1]] == 0 and road[n2[0]][n2[1]] == 0:
-            poss.append((n1,n2))
+            pos.append((n1, n2))
+        
+    if q1[0] == q2[0]:
+        for i in [-1,1]:
+            if road[q1[0]+i][q1[1]] == 0 and road[q2[0]+i][q2[1]] == 0:
+                pos.append((q1, (q1[0] + i, q1[1])))
+                pos.append((q2, (q2[0] + i, q2[1])))
+    else:
+        for i in [-1,1]:
+            if road[q1[0]][q1[1]+i] == 0 and road[q2[0]][q2[1]+i] == 0:
+                pos.append(((q1[0], q1[1] + i), q1))
+                pos.append(((q2[0], q2[1] + i), q2))
+    return pos
             
-    #회전        
-    if p1[0] == p2[0]: #수평일때
-        for i in [-1,1]:
-            if road[p1[0]+i][p1[1]] == 0 and road[p2[0]+i][p2[1]] == 0:
-                poss.append((p1, (p1[0]+i,p1[1])))
-                poss.append((p2, (p2[0]+i,p2[1])))
-    else: # 수직일때
-        for i in [-1,1]:
-            if road[p1[0]][p1[1]+i] == 0 and road[p2[0]][p2[1]+i] == 0:
-                poss.append(((p1[0],p1[1]+i),p1))
-                poss.append(((p2[0],p2[1]+i),p2))
-    return poss
-
 def solution(board):
-    length = len(board)
-    #  범위 밖으로 못나가게 벽을 만듬
-    road= [[1] * (length+2) for _ in range(length+2)]
-    for i in range(length):
-        for j in range(length):
+    n = len(board)
+    road = [[1] * (n+2) for _ in range(n+2)]
+    for i in range(n):
+        for j in range(n):
             road[i+1][j+1] = board[i][j]
-
-    que = deque([((1, 1), (1, 2), 0)])
-    confirm = set([((1, 1), (1, 2))])
-    while que:
-        p1, p2, result = que.popleft()
-        if p1 == (length,length) or p2 == (length,length):
-            return result
-        for k in move(p1,p2,road):
-            if k not in confirm:
-                que.append((*k, result+1))
-                confirm.add(k)
+    
+    q  = deque([((1,1), (1,2), 0)])
+    confirm = set([((1,1), (1,2))])
+    
+    while q:
+        q1, q2, cnt = q.popleft()
+        if q1 == (n,n) or q2 == (n, n):
+            return cnt
+        for nxt in move(q1, q2, road):
+            if nxt not in confirm:
+                q.append((*nxt, cnt+1))
+                confirm.add(nxt) 
