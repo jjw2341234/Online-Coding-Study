d4060eef1ff227d8e372f2937984d2360eaf251a
jjw2341234 <jjw2341234@gmail.com>
2025-03-10 10:59:51 +0900

    [Silver IV] Title: 한수, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,7 @@
+num = int(input())
+cnt = 0
+for i in range(1,num+1):
+    num_list = list(map(int, str(i)))
+    if i<100 or num_list[0] - num_list[1] == num_list[1] - num_list[2]:
+        cnt+=1
+print(cnt)
