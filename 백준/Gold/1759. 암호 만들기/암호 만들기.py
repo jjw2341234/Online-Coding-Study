460123162b0e4114dc505dcb1d314a043e284c74
jjw2341234 <jjw2341234@gmail.com>
2025-03-11 10:43:55 +0900

    [Gold V] Title: 암호 만들기, Time: 140 ms, Memory: 111412 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,24 @@
+n,m = map(int, input().split())
+
+str = list(input().split())
+str.sort()
+res_list = []
+def pwd(x, res):
+    global res_list
+    if len(res) == n:
+        rcnt = 0
+        for r in res:
+            if "a" == r or "e" == r or "i" == r or "u" == r or "o" == r:
+                rcnt+=1
+        if n-rcnt >= 2 and rcnt:
+            if ''.join(res) not in res_list:
+                res_list.append(''.join(res))
+            return 
+    for i in range(x, len(str)):
+        if str[i] not in res:
+            res.append(str[i])
+            pwd(i+1, res)
+            res.pop()
+pwd(0, [])
+for r in res_list:
+    print(r)
