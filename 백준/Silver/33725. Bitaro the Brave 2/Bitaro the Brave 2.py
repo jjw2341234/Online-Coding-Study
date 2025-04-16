e5a1d93c3b2a9e918c094161ba4546d56a643b1a
jjw2341234 <jjw2341234@gmail.com>
2025-04-16 15:02:04 +0900

    [Silver I] Title: Bitaro the Brave 2, Time: 324 ms, Memory: 222404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/33725. Bitaro the Brave 2/Bitaro the Brave 2.py
+++ 백준/Silver/33725. Bitaro the Brave 2/Bitaro the Brave 2.py
@@ -1,43 +1,33 @@
 import sys
 
-input = sys.stdin.readline
+N = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
 
-n = int(input())
+c = [0] * (N+1)
+d = [0] * (N)
 
-arr = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
+for i in range(N):
+    c[i+1] = c[i] + b[i]
+    d[i] = a[i] - c[i]
 
-def can_clear(x, a,b, n):
-    for j in range(n):
-        power = x
-        success = True
+leftm = [0] * N
+rightm = [0] * N
 
-        for i in range(j, n):
-            if power < a[i]:
-                success = False
-                break
-            power+= b[i]
-        
-        if success:
-            for i in range(j):
-                if power < a[i]:
-                    success = False
-                    break
-                power+= b[i]
-        
-        if success:
-            return True
-    return False
+leftm[0] = d[0]
+rightm[N-1] = d[N-1]
 
+#현재 leftm의 경우, leftm의 값과 d의 값을 비교해서 좋은 값을 가져오는것이 목표이다.
 
-lo = 0
-hi = int(10**9)
+for i in range(1, N):
+    leftm[i] = max(leftm[i-1], d[i-1])
 
-while lo < hi:
-    mid = (lo + hi) //2
-    if can_clear(mid, arr, arr2, n):
-        hi  = mid
-    else:
-        lo = mid+1
-print(lo)
+for i in range(N-2, -1, -1):
+    rightm[i] =max(rightm[i+1],d[i])
 
+ans = float("inf")
+
+for i in range(N):
+    k = max(leftm[i]- c[N], rightm[i]) + c[i]
+    ans = min(k, ans)
+print(ans)
