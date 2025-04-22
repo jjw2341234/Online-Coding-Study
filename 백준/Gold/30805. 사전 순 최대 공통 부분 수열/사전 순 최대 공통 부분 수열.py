ffddd2e21feb83b06c1cacda232754f6bd7253f7
jjw2341234 <jjw2341234@gmail.com>
2025-04-22 10:44:20 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
@@ -1,17 +1,16 @@
 n = int(input())
-a = list(map(int, input().split()))
+s1 = list(map(int, input().split()))
 m = int(input())
-b = list(map(int, input().split()))
-common = set(a) & set(b)
+s2 = list(map(int, input().split()))
+com = set(s1) & set(s2)
 res = []
-while common:
-    max_value = max(common)
-
-    ind1 = a.index(max_value)
-    ind2 = b.index(max_value)
-    res.append(max_value)
-    a = a[ind1+1:]
-    b = b[ind2+1:]
-    common = set(a) & set(b)
+while com:
+    maxv = max(com)
+    max_s1_ind = s1.index(maxv)
+    max_s2_ind = s2.index(maxv)
+    res.append(maxv)
+    s1 = s1[max_s1_ind+1:]
+    s2 = s2[max_s2_ind+1:]
+    com = set(s1)&set(s2)
 print(len(res))
-print(' '.join(map(str, res)))
+print(*res)
