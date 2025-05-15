8140383a8d4cdf07e22ebecf46e6b49d0d080dc2
jjw2341234 <jjw2341234@gmail.com>
2025-05-15 11:48:12 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 916 ms, Memory: 166864 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,46 +1,31 @@
 from collections import deque
+
 r,c,t = map(int, input().split())
+
 arr = [list(map(int, input().split())) for _ in range(r)]
+
 dust = []
-cleaner =[]
+machine = []
 
-def diffusion():
+def diffuse():
     while dust:
-        x,y,val = dust.pop()
+        x,y, val = dust.pop()
         cnt = 0
-        for nx, ny in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
-            if 0<=nx<r and 0<=ny< c and [nx, ny] not in cleaner:
-                arr[nx][ny] += val // 5
+        for nx, ny in ([x+1, y], [x-1, y], [x, y-1], [x, y+1]):
+            if 0<=nx<r and 0<=ny < c and (nx, ny) not in machine:
+                arr[nx][ny]+= val//5
                 cnt+=1
-        arr[x][y]-= (val//5) * cnt
-
-def clean_up():
-    dx = [0, -1, 0, 1]
-    dy = [1, 0, -1, 0]
-    x,y, = cleaner[0]
-    dir = 0
-    up, y = x, 1
-    prev = 0
-    while True:
-        nx, ny = x+dx[dir], y+dy[dir]
-        if nx == -1 or ny == -1 or nx == r or ny == c:
-            dir = (dir+1) % 4
-            continue
-        if x == up and y == 0:
-            break
-        arr[x][y], prev = prev, arr[x][y]
-        x,y = nx, ny
-    return
+        arr[x][y] -= val//5 * cnt
 
 def clean_down():
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-    x,y, = cleaner[1]
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
+    x,y = machine[1] # 여기는 가면 안된다
     dir = 0
-    down, y = x, 1
+    down,y = x,1
     prev = 0
     while True:
-        nx, ny = x+dx[dir], y+dy[dir]
+        nx, ny = x + dx[dir], y + dy[dir]
         if nx == -1 or ny == -1 or nx == r or ny == c:
             dir = (dir+1) % 4
             continue
@@ -53,18 +38,36 @@ def clean_down():
 for i in range(r):
     for j in range(c):
         if arr[i][j] > 0:
-            dust.append([i,j, arr[i][j]]) #위치와 값을 동시에 매핑
+            dust.append((i,j, arr[i][j]))
         elif arr[i][j] == -1:
-            cleaner.append([i,j])
+            machine.append((i,j))
+
+def clean_up():
+    dx = [0,-1,0,1]
+    dy = [1,0,-1,0]
+    x,y = machine[0] # 여기는 가면 안된다
+    dir = 0
+    up,y = x,1
+    prev = 0
+    while True:
+        nx, ny = x + dx[dir], y + dy[dir]
+        if nx == -1 or ny == -1 or nx == r or ny == c:
+            dir = (dir+1) % 4
+            continue
+        if x == up and y == 0:
+            break
+        arr[x][y], prev = prev, arr[x][y]
+        x,y = nx, ny
+    return
 for _ in range(t):
-    diffusion()
+    diffuse()
     clean_up()
     clean_down()
-    dust = []
     for i in range(r):
         for j in range(c):
-            if arr[i][j] > 0:
-                dust.append([i,j, arr[i][j]]) #위치와 값을 동시에 매핑
+            if arr[i][j]>0:
+                dust.append([i,j, arr[i][j]])
+
 ans = 0
 for i in range(r):
     for j in range(c):
