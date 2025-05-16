f4ca2354d659fc895ef5feb07221a9b8888f3f36
jjw2341234 <jjw2341234@gmail.com>
2025-05-16 11:26:36 +0900

    [Gold III] Title: 치즈, Time: 196 ms, Memory: 116588 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,16 +1,18 @@
 from collections import deque
 
-n,m = map(int, input().split())
-arr = [list(map(int, input().split())) for i in range(n)]
+moi = lambda : map(int, input().split())
+
+n,m = moi()
+arr = [list(moi()) for _ in range(n)]
 
 def bfs():
     q = deque()
     q.append([0,0])
     visit[0][0] = 1
     while q:
-        x,y = q.popleft()
-        for nx, ny in [(x+1, y), (x-1, y), (x,y+1), (x,y-1)]:
-            if 0<=nx<n and 0<=ny<m and not visit[nx][ny]:
+        x,y, = q.popleft()
+        for nx, ny in [(x+1, y), (x-1, y), (x, y-1), (x, y+1)]:
+            if 0<=nx< n and 0<=ny<m and not visit[nx][ny]:
                 if arr[nx][ny]>=1:
                     arr[nx][ny]+=1
                 else:
@@ -19,15 +21,15 @@ def bfs():
 
 time = 0
 while 1:
-    visit = [[0] * m for _ in range(n)]
-    bfs()
+    visit = [[0] * (m) for _ in range(n)]
     flag = 0
+    bfs()
     for i in range(n):
         for j in range(m):
-            if arr[i][j] >=3:
-                arr[i][j] = 0
+            if arr[i][j] >= 3:
                 flag = 1
-            elif  arr[i][j] == 2:
+                arr[i][j] = 0
+            elif arr[i][j] == 2:
                 arr[i][j] = 1
     if flag == 1:
         time+=1
