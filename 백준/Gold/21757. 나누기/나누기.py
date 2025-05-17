2e3b978295097023c44f9a991e0c640b3e57a120
jjw2341234 <jjw2341234@gmail.com>
2025-05-17 10:43:27 +0900

    [Gold II] Title: 나누기, Time: 116 ms, Memory: 121200 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.py
+++ 백준/Gold/21757. 나누기/나누기.py
@@ -1,5 +1,3 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 prefix_sum = 0
@@ -9,17 +7,15 @@ ans = 0
 
 targert = sum(arr)//4
 
+
 if sum(arr) % 4:
     print(0)
     exit()
 
 for i in range(len(arr)-1):
     prefix_sum+= arr[i]
-    if prefix_sum == targert:
-        cnt1+=1
-    if prefix_sum == targert * 2:
-        cnt2+=cnt1
-    if prefix_sum == targert * 3:
-        ans+=cnt2 #마지막의 경우, 그전의 값들의 갯수에 따라 달라질수 밖에 없다.
+    if prefix_sum == targert * 3: ans+=cnt2 #마지막의 경우, 그전의 값들의 갯수에 따라 달라질수 밖에 없다.
+    if prefix_sum == targert * 2: cnt2+=cnt1
+    if prefix_sum == targert: cnt1+=1
 
 print(ans)
