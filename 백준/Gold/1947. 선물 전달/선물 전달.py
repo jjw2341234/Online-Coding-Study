d1fa7b6259fc0f7c9c3bedb72ee897092ce94b2a
jjw2341234 <jjw2341234@gmail.com>
2025-06-19 22:08:45 +0900

    [Gold III] Title: 선물 전달, Time: 100 ms, Memory: 117360 KB -BaekjoonHub

--- 백준/Gold/1947. 선물 전달/선물 전달.py
+++ 백준/Gold/1947. 선물 전달/선물 전달.py
@@ -1,12 +1,9 @@
 n = int(input())
-MOD = 10**9
+max_num = 10**9
+arr = [0] * (n+1)
+arr[1] = 0
+if n>=2:arr[2] = 1
 
-dp = [0] * (n + 1)
-dp[1] = 0
-if n >= 2:
-    dp[2] = 1
-
-for i in range(3, n + 1):
-    dp[i] = (dp[i - 1] + dp[i - 2]) * (i - 1) % MOD
-
-print(dp[n])
+for i in range(3, n+1):
+    arr[i] = (arr[i-2] + arr[i-1]) * (i-1) % max_num
+print(arr[n])
